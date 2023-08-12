fd360d011812ba54e1dc70040fee81eae38ed6dc
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-11 17:32:58 -0700

    task 7 var,if,else,while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: alaways 0 (success)
+  */
+int main(void)
+{
+	char c = 'z';
+	int i;
+
+	for (i = 0; i < 26; ++i)
+		putchar(c--);
+	putchar(10);
+	return (0);
+}
