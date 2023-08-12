5e6b5ca45ce2ba369c14b3a389cb2e68e63f4871
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-11 17:49:28 -0700

    task 9 var,if,else,while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; ++i)
+	for (i = 0; i < 10; ++i)
 	{
 		putchar(48 + i);
+		if (i == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('9');
+	putchar(10);
 	return (0);
 }
