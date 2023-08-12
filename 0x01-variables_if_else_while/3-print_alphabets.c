49abddd79eb489a6fcb749c12c020a1df6999bbe
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-11 17:02:09 -0700

    task 3 var,if,else,while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: alaways 0 (success)
+  */
+int main(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 26; ++i)
+		putchar(c++);
+	putchar("\n");
+	return (0);
+}
