2c42bbf77b95d5629dc00c97e71dba8c6c4ff330
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-15 14:25:07 -0700

    task 2 c,func,nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
   *
   * Return: no return value (void func)
   */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 
--- 0x02-functions_nested_loops/2-print_alphabet_x10.h
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.h
@@ -1,5 +1,5 @@
 #ifndef PRINT_ALPHABET_X10_H_
 #define PRINT_ALPHABET_X10_H_
 
-void print_alphabet_10x(void);
+void print_alphabet_x10(void);
 #endif
