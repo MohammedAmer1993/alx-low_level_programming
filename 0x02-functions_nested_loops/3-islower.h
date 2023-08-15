e36cd484b22bc205d79bca0838fe4c31e73d7c2d
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-15 15:03:32 -0700

    task 3 c,function,nested loops

---
+++ 0x02-functions_nested_loops/3-islower.h
@@ -0,0 +1,5 @@
+#ifndef IS_LOWER_H_
+#define IS_LOWER_H_
+
+int _islower(char c);
+#endif
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 #include "_putchar.h"
 #include "1-alphabet.h"
 #include "2-print_alphabet_x10.h"
+#include "3-islower.h"
 #endif
