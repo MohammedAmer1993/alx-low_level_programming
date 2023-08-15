69225e7ea092ed8af75d4407260a6bdd0d398c89
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-15 16:34:54 -0700

    task 5 c,func,nested loops

---
+++ 0x02-functions_nested_loops/5-sign.h
@@ -0,0 +1,5 @@
+#ifndef _IS_ALPHA_H_
+#define _IS_ALPHA_H_
+
+int print_sign(int n);
+#endif
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,4 +6,5 @@
 #include "2-print_alphabet_x10.h"
 #include "3-islower.h"
 #include "4-isalpha.h"
+#include "5-sign.h"
 #endif
