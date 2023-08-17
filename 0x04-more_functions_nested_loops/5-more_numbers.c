f59933905b625c357caa86ffc09e2ef015a344d3
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-17 15:53:04 -0700

    task 5 more func

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "_putchar.h"
+/**
+  * more_numbers - func to print to 14
+  * Return: nothing (void)
+  */
+void more_numbers(void)
+{
+	int i;
+	int f;
+	int s;
+	int tmp;
+
+	for (i = 0; i < 15; ++i)
+	{
+		tmp = i;
+		f = tmp % 10;
+		tmp = tmp / 10;
+		s = tmp % 10;
+		if (i > 9)
+			_putchar(s + '0');
+		_putchar(f + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 #include "_putchar.h"
+void more_numbers(void);
 void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
