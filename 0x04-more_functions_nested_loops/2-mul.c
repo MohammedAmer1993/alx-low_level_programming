b090ae7003a1c88ec07be11eaac6c0077b0e724e
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-17 02:36:00 -0700

    task 2 more func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+  * mul - func to multiply 2 int
+  * @a: frist int
+  * @b: second int
+  * Return: the result (int)
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
 #endif
