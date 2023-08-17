300d9aa1edae2f8eba45ee3ac72cd5ccf8b13cd0
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-16 17:05:41 -0700

    task 3 last one C debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 void positive_or_negative(void);
 #endif
