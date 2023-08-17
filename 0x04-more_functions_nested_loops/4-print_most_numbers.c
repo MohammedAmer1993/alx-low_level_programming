48380824167c4c32105d28494d40d7e07070d2bf
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-17 11:00:00 -0700

    task 4 more func

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+/**
+  * print_numbers - print from 0 to 9
+  *
+  * Return: nothing (void)
+  */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
