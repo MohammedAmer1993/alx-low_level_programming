1c496fab92bce3218c9b28131ee8a8528e1a0228
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-17 17:00:55 -0700

    task 8 more func

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "_putchar.h"
+/**
+  * print_square - print a shape of diagonal
+  * @size: height of diagonal
+  * Return: nothing (void)
+  */
+void print_square(int size)
+{
+	int i;
+	int z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; ++i)
+		{
+			for (z = 0; z < size; ++z)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
