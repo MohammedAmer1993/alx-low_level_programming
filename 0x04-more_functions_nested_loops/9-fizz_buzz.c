bcc2649f2bfb44d57848d9388dcdbd1ce87201d3
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-17 17:37:12 -0700

    task 9 fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,16 +7,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; ++i)
+	printf("%d", 1);
+	for (i = 2; i < 101; ++i)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
