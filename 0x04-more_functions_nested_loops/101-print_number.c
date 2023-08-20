6f0fd5ce9d880c1969bd6dc3bb2d3e8755ed56f5
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-20 15:44:36 -0700

    the last task in more function proj done after sec deadline

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "_putchar.h"
+/**
+  * print_number - print a number using _putchar
+  * @n: number to be printed
+  * Return: nothing (void)
+  */
+void print_number(int n)
+{
+	int val;
+	int dv = 1000000000;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	while (n / dv == 0)
+		dv = dv / 10;
+	while (dv >= 10)
+	{
+		val = n / dv;
+		if (val == 0)
+		{
+			_putchar('0' + val);
+			dv = dv / 10;
+		}
+		else
+		{
+			n = n - (val * dv);
+			dv = dv / 10;
+			_putchar('0' + val);
+		}
+	}
+	_putchar('0' + n);
+}
