fe65c317e8d5e8e078384c9f29d22f1c0889ac18
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-21 07:44:22 -0700

    task 5 pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+  * rev_string - func to reverse a string
+  * @s: pointer to string
+  * Return: Nothing (void func)
+  */
+void rev_string(char *s)
+{
+	char tmpchar = 0;
+	char *tmp = s;
+
+	while (*tmp)
+	{
+		++tmp;
+	}
+	--tmp;
+	while (tmp > s)
+	{
+		tmpchar = *s;
+		*s = *tmp;
+		*tmp = tmpcahr;
+		++s;
+		--tmp;
+	}
+}
