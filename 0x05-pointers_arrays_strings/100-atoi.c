1036980fcaff033f773e41228517e8bd97f6cb15
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-21 15:34:25 -0700

    task 10 advanced pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+/**
+  * _atoi - conver string into int
+  * @s: string to be converted
+  * Return: the intger val
+  */
+int _atoi(char *c)
+{
+	int j = 1;
+	int i = 0;
+	int tmp = 0;
+
+	while (*c)
+	{
+		if (*c == '-')
+		{
+			j *= -1;
+			++c;
+		}
+		else if (*c == '+' || *c == ' ')
+		{
+			++c;
+			continue;
+		}
+		else if ((*c < 48 || *c > 57) && *c != '+' && *c != ' ')
+		{
+			j = 1;
+			++c;
+		}
+		else if (*c > 47 && *c < 58)
+		{
+			while (*c > 57 && *c < 58)
+			{
+				tmp = *c + '0';
+				i = 10 * i + tmp;
+				++c;
+			}
+			return (i * j);
+		}
+	}
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 #include "_putchar.h"
 void puts2(char *str);
+int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
