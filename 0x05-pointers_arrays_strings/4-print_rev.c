d09606a9f1fd7bfdb228022c7a2f3cd92c7ee4a9
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-20 21:13:29 -0700

    task 4 pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "_putchar.h"
+/**
+  * print_rev - prints reversed str
+  * @s: the print to be reversed
+  * Return: no return void func
+  */
+void print_rev(char *s)
+{
+	char *tmp = --s;
+
+	while (*s)
+		++s
+	--s;
+	while (s != tmp)
+	{
+		_putchar(*s);
+		--s;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 #include "_putchar.h"
 void _puts(char *str);
+void print_rev(char *s);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
