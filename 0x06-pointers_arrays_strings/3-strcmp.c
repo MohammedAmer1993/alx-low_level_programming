92122277418161efdcd532eaa9073749415aa481
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-23 16:42:35 -0700

    task 3 more pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/**
+  * _strcmp - compare two string
+  * @s1: pointer to frist string
+  * @s2: pointer to second string
+  * Return: 0 if match + if s1 greater, - if s2 greater
+  *
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int len;
+	int f = 0;
+
+	len = strlen(s1) + 1;
+	while (i < len)
+	{
+		f = s1[i] - ss[i];
+		if (f != 0)
+			return (f);
+		++i;
+	}
