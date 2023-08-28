f74a311602ef5be6ec44f5468550f0d754faeb91
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-28 07:08:42 -0700

    task 2 proj more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+  * _strchr - find a char
+  * @s: the search buffer
+  * @b: the char to find
+  * Return: pointer to char if found | NULL;
+  */
+char *_strchr(char *s, char b)
+{
+	char *f = s;
+
+	while (*s)
+	{
+		if ((*f) == b)
+		{
+			return (f);
+		}
+		++f;
+		++s;
+	}
+	return (0);
+}
