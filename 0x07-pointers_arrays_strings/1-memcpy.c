584ef3d41d62cbebeb8b88247ed33456f918e4c7
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-28 06:53:16 -0700

    task 1 project more pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+  * _memcpy - copies a buffer to n bytes
+  * @dest: the destination buffer
+  * @src: the sourece buffer
+  * @n: number of bytes
+  * Return: pointer to frist element of dest buffer 
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while(i < n)
+	{
+		*(dest + i) = *(src + i);
+		++i;
+	}
+	return (dest);
+}
