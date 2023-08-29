cf22006d02d46f0b8f77669b56ae0a45f858e382
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-28 18:54:28 -0700

    task 8 adva more pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+  * set_string - sets the address of a string to another address
+  * @s: the string which will get a new address
+  * @to: the new address
+  * Return: nothing void func
+  */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
