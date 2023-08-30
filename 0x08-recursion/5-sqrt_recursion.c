a482f310d4c6a84f4f5f3bacccfb452a9551e86f
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-30 16:52:49 -0700

    testing something in task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,26 @@
+
 /**
   * _sqrt_recursion - calculate the root
   * @n: the number
   * Return: the root
   */
+int sqrtmine(int guess, int n)
+{
+	int guessi = 0;
+	guessi = (guess + n/guess) / 2;
+	if (guessi * guess == n)
+	{
+		return (guess);
+	}
+	if (guessi - guess < 1)
+	{
+		return (-1);
+	}
+	return sqrtmine(guessi, n);
+}
 int _sqrt_recursion(int n)
+{
+	if (n < 2)
+		return (1);
+	return (sqrtmine(2, n));
+}
