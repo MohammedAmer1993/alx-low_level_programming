1da4422f0d41a56c2f658bd9a992ce2146da0416
MohammedAmer <loard.amer.2010@gmail.com>
2023-08-30 17:13:54 -0700

    hope this will work task 5 sqare root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,37 @@
+/**
+  * sqrtmine - func to help find square root
+  * @guess: the guess
+  * @n: the number
+  * Return: the square root
+  */
+
+int sqrtmine(int guess, int n)
+{
+	int guessi = 0;
+
+	guessi = (guess + n / guess) / 2;
+	if (guessi * guess == n)
+	{
+	return (guess);
+	}
+	if (guessi >= guess)
+	{
+	return (-1);
+	}
+	return (sqrtmine(guessi, n));
+}
 
 /**
   * _sqrt_recursion - calculate the root
   * @n: the number
   * Return: the root
   */
-int sqrtmine(int guess, int n)
-{
-	int guessi = 0;
-	guessi = (guess + n/guess) / 2;
-	if (guessi * guess == n)
-	{
-		return (guess);
-	}
-	if (guessi - guess < 1)
-	{
-		return (-1);
-	}
-	return sqrtmine(guessi, n);
-}
+
 int _sqrt_recursion(int n)
 {
+	if (n <= 0)
+		return (-1);
 	if (n < 2)
 		return (1);
-	return (sqrtmine(2, n));
+	return (sqrtmine(n - 1, n));
 }
