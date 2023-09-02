f3d1a94f1bddbc69a6339f61058862e1770bbf55
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-01 18:34:28 -0700

    task 3 argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - entry point to multiplication prog
+  * @argc: number of arg passed
+  * @argv: array of strings contain arg
+  * Return: zero if success and 1 if fail
+  */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
