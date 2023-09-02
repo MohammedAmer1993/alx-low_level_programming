aece3f5e284230f4d7815199a1d8a201ccb117fa
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-01 18:06:48 -0700

    task 0 argc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * @argc: number of arg passed
+  * @argv: array of strings contain arg
+  * Return: alaways zero (success)
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is readme file for argc and argv project
