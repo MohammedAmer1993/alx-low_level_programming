bcae4029c662840941c6b7e17d8ab6484d08d583
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-05 07:45:48 -0700

    tt

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
   * alloc_grid - func to build a grid
   * @width: width of the grid
@@ -6,5 +7,30 @@
   */
 int **alloc_grid(int width, int height)
 {
+	int **ptr;
+	int i;
+	int z;
 
+	if (width <= 0)
+		return (0L);
+	if (height <= 0)
+		return (0L);
+	ptr = (int **) malloc(width * sizeof(int *));
+	if (!ptr)
+		return (0L);
+	for (i = 0; i < width; ++i)
+	{
+		*(ptr + i) = (int *) malloc(height * sizeof(int));
+		if(!(*(ptr + i)))
+			return (0L);
+		++i;
+	}
+	for (i = 0; i < width; ++i)
+	{
+		for (z = 0; z <height; ++z)
+		{
+			ptr[i][z] = 0;
+		}
+	}
+	return (ptr);
 }
