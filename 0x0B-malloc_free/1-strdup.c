32a93586f4820b30cf986bef8d075620be34dc6e
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-05 05:37:08 -0700

    task 1 malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 /**
   * _strdup- dublicate a string into heap
   * @str: the orginal string in stack
@@ -5,5 +7,20 @@
   */
 char *_strdup(char *str)
 {
+	int len;
+	char *ptr;
+	int i;
 
+	len = strlen(str) + 1;
+	ptr = (char *) malloc(len);
+	if (!ptr)
+		return (0L);
+	i = 0;
+	while (str[i])
+	{
+		ptr[i] = str[i];
+		++i;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
