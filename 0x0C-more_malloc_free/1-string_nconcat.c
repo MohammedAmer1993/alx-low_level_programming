ba38af4a6118ee3c98872352957f6f5ded5da6e4
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-07 15:48:47 -0700

    task 1 more alloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdlib.h>
 /**
   * string_nconcat - concat n bytes of s2 with s1
@@ -8,4 +9,30 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int l1, l2, i, m;
+	char *ptr;
+
+	if (s1)
+		l1 = strlen(s1);
+	else
+		l1 = 0;
+	if (s2)
+		l2 = strlen(s2);
+	else
+		l2 = 0;
+	if (n > l2)
+		n = l2;
+	ptr = malloc(l1 + n + 1);
+	if (!ptr)
+		return (0L);
+	for (i = 0; i < l1; ++i)
+		ptr[i] = s1[i];
+	m = i;
+	for (i = 0; i < n; ++i)
+	{
+		ptr[m] = s2[i];
+		++m;
+	}
+	ptr[m] = '\0';
+	return (ptr);
 }
