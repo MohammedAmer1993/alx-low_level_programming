3d3faef483cd7b35b81fdb0dbfb607c09947f933
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-07 16:03:56 -0700

    task 2 more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,22 @@
 #include <stdlib.h>
 /**
-  * _calloc - allocates memory 
+  * _calloc - allocates memory
   * @nmemb: number of elments
   * @size: size of element
   * Return: address of allocated mem
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	int i;
+	int tot_size = nmemb * size;
+
+	if (!tot_size)
+		return (0L);
+	ptr = malloc(tot_size);
+	if (!ptr)
+		return (0L);
+	for (i = 0; i < tot_size; ++i)
+		((char *)ptr)[i] = 0;
+	return (0L);
 }
