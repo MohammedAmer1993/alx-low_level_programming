ae4a3d7a4d5e7543f7a39d9be01a418b374c9b99
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-11 10:01:43 -0700

    task 0 struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+  * struct dog - dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
