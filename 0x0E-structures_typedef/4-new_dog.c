e026a39b95e12dc5387ba06cc4f7a5312da8ecb0
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-11 17:01:05 -0700

    task 4 struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
   * new_dog - allocating mem for new dog
   * @name: name of the dog
@@ -24,8 +25,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (!d)
 		return (0L);
+	d->name = malloc(strlen(name) + 1);
+	if (!(d->name))
+	{
+		free(d);
+		return (0L);
+	}
 	d->name = cp_name;
 	d->age = age;
+	d->owner = malloc(strlen(owner) + 1);
+	if (!(d->owner))
+	{
+		free(d->name);
+		free(d);
+		return (0L);
+	}
 	d->owner = cp_owner;
 	return (d);
 }
