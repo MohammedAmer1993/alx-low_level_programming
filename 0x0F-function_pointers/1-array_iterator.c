a4e66cff56f491676fecf2f60ea6b591bddd1fbe
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-13 09:00:51 -0700

    task 1 func pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+  * array_iterator - iterate on array and choose func
+  * @array: the array
+  * @size: size of array
+  * @action: the func to be called
+  * Return: nothing
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !size || !action)
+		return;
+	for (i = 0; i < size; ++i)
+	{
+		action(array[i]);
+	}
+}
