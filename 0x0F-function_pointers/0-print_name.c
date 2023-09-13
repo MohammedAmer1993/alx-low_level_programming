0b7acfdae771ec16a89ed9863733f34f7e29fac8
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-13 08:37:03 -0700

    task 0 func pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 /**
   * print_name - prints a name in two modes normal and upper
   * @name: the name to be printed
@@ -7,5 +7,5 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f();
+	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTER_H_
 #define FUNCTION_POINTER_H_
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
