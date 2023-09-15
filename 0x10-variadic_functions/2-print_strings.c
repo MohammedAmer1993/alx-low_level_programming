2eac950f8a843dbea8f93846eefe4ed65a04ec9d
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-14 18:40:10 -0700

    task 2 variadic func

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	char *tmp;
 
-	if (n == 0)
+	if (n == 0 && separator)
 	{
 		printf("%s\n", separator);
 		return;
