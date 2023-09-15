95fbf2a6889df32cdb07f26d5b8d4c94ab16b794
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-14 17:58:46 -0700

    task 3 variadic func

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0 && (separator == 0 || *separator == '\0'))
+	{
+		printf("\n");
 		return;
+	}
 	if (n == 0)
 	{
 		printf("%s\n", separator);
