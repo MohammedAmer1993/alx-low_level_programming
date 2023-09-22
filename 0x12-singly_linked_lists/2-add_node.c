c10ff62905af27fd14b1149b7be1919f70c8d203
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-21 18:30:49 -0700

    task 2 linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,11 +10,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *tmp;
+	int i;
 
 	if (*head)
 		return (0L);
+	for (i = 0; str[i]; ++i)
+	{
+	}
 	tmp = malloc(sizeof(tmp));
 	tmp->str = strdup(str);
+	tmp->len = i;
 	tmp->next = (*head)->next;
 	*head = tmp;
 	return (*head);
