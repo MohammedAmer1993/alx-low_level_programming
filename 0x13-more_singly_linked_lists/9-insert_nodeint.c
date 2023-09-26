4d9200ab30885f842f602bfabc719add1e1c7222
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-26 14:51:21 -0700

    task 9 more linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,4 +8,28 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int i;
+	listint_t *tmp = *head;
+	listint_t *tmp2 = malloc(sizeof(listint_t));
+
+	if (!tmp2)
+		return (0L);
+	if (*head == 0L)
+	{
+		tmp2->n = n;
+		tmp2->next = 0L;
+		*head = tmp2;
+		return (*head);
+	}
+	for (i = 0; i < idx - 1; ++i)
+	{
+		if (tmp->next)
+			tmp = tmp->next;
+		else
+			return (0L);
+	}
+	tmp2->next = tmp->next;
+	tmp2->n = n;
+	tmp->next = tmp2;
+	return (*head);
 }
