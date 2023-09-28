b405c2b25a7e92b18fbeb11df910ecb92acff0d9
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-28 16:38:21 -0700

    task 5 bit main

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,16 @@
 #include "main.h"
-
+/**
+  * get_bit - to get a certain bit
+  * @n: the number
+  * @index: the bit you want
+  * Return: the bit
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+        if (index > 63)
+                return (-1);
+        return (((1 << index) & n) >> index);
+}
 /**
   * flip_bits - to flip a cetain bit
   * @n: the number
@@ -8,4 +19,13 @@
   */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int i;
+	int z = 0;
+
+	for (i = 0; i < 64; ++i)
+	{
+		if (get_bit(n, i) != get_bit(m, i))
+			z++;
+	}
+	return (z);
 }
