ae8c90b591be7722c5a7487d08b7f208520c4a52
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-28 17:06:23 -0700

    task 6 adv bit main

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * get_indianness - know the endianness of cpu
+  * Return: 1 if little endian, 0 if big endian
+  */
+int get_endianness(void)
+{
+	unsigned int x = 0xaabbccdd;
+	unsigned char y = (unsigned char) x;
+
+	if (y == 0xdd)
+		return (1);
+	else
+		return (0);
+}
