656ccf939183b00c01f3e8ca86285be6e2e3fd25
MohammedAmer <loard.amer.2010@gmail.com>
2023-10-02 18:46:12 -0700

    task 2 file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,7 @@ int create_file(const char *filename, char *text_content)
 			if (write(fd, text_content, strlen(text_content)) < 0)
 				return (-1);
 	}
+	close(fd);
 	return (1);
 
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,4 +7,20 @@
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
+
+	if (text_content == NULL)
+	{
+		if (filename == NULL)
+			return (-1);
+		else
+			return (1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (write(fd, text_content, strlen(text_content)) < 0)
+		return (-1);
+	close(fd);
+	return (1);
 }
