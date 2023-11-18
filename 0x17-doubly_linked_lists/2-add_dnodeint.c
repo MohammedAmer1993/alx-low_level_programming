bbde82a36ee99a9d9443ecf91e45f707ed1232cc
Mohammed Amer <loard.amer.2010@gmail.com>
2023-11-18 14:59:45 +0200

    task 2 doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp = malloc(sizeof(dlistint_t));
 
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
 	tmp->n = n;
 	tmp->next = NULL;
 	tmp->prev = NULL;
