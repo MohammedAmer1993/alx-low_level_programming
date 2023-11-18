6dff47ccab0889195cf511ac78a9cb61e1bc959b
Mohammed Amer <loard.amer.2010@gmail.com>
2023-11-18 14:18:43 +0200

    task 0 doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,5 +7,27 @@
   */
 size_t print_dlistint(const dlistint_t *h)
 {
+	int i = 0;
 
+	if (!h)
+	{
+		return (0);
+	}
+	while (h->next)
+	{
+		h = h->next;
+	}
+	if (h->prev == NULL)
+	{
+		printf("%d\n", h->n);
+		return (1);
+	}
+	while (h->prev)
+	{
+		printf("%d\n", h->n);
+		++i;
+		h = h->prev;
+	}
+	printf("%d\n", h->n);
+	return (++i);
 }
