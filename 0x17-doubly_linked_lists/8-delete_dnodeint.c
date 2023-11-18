8d3c5e3b919bad8d3e2001127b7bb2c68f39c250
Mohammed Amer <loard.amer.2010@gmail.com>
2023-11-19 00:48:23 +0200

    task 8 doubly linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,6 +9,38 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *tmp = *head;
+	dlistint_t *tmp2;
+	unsigned int i;
 
+	if (tmp == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+	for (i = 0; i < index; ++i)
+		if (tmp->next)
+			tmp = tmp->next;
+		else
+		{
+			return (-1);
+		}
+	tmp2 = tmp->prev;
+	if (tmp->next)
+	{
+		tmp->next->prev = tmp2;
+		tmp2->next = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		tmp2->next = NULL;
+		free(tmp);
+		return (1);
+	}
 }
 
