f8157485aa483ad7ccf0f7761dd50d71910050fe
Mohammed Amer <loard.amer.2010@gmail.com>
2023-12-03 00:56:08 +0200

    task 0 hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,5 +7,10 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
 
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = NULL;
 }
