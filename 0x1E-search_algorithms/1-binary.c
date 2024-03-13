dd72051a1fe5ab83d96ddbed9d87958aadf1930d
Mohammed Amer <loard.amer.2010@gmail.com>
2024-03-13 17:17:49 +0200

    task 2 search algor

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,4 +10,37 @@
 
 int binary_search(int *array, size_t size, int value)
 {
+	size_t start = 0;
+	size_t stop = size - 1;
+	size_t middle = 0;
+	size_t i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+
+	while (start < stop)
+	{
+		middle = (start + stop) / 2;
+		printf("Searching in array: %d", array[start]);
+		for (i = start + 1; i <= stop; ++i)
+		{
+			printf(", %d", array[i]);
+		}
+		printf("\n");
+		if (array[middle] == value)
+		{
+			return (middle);
+		}
+		else if (array[middle] > value)
+		{
+			stop = middle - 1;
+		}
+		else if (array[middle] < value)
+		{
+			start = middle + 1;
+		}
+	}
+	return (-1);
 }
